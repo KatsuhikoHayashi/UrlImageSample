0f2a55e0856d09cb9002c0a4cb0fd506bf16ff22
hayashidesu <katsu-h@w4.dion.ne.jp>
2015-07-21 16:06:42 +0900

    Sample

--- UrlImageSample/AppDelegate.swift
+++ UrlImageSample/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  UrlImageSample
 //
-//  Created by 林克彦 on 2015/07/21.
+//  Created by Hayashidesu. on 2015/07/21.
 //  Copyright (c) 2015年 Hayashidesu. All rights reserved.
 //
 
--- UrlImageSample/ViewController.swift
+++ UrlImageSample/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  UrlImageSample
 //
-//  Created by 林克彦 on 2015/07/21.
+//  Created by Hayashidesu. on 2015/07/21.
 //  Copyright (c) 2015年 Hayashidesu. All rights reserved.
 //
 
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string:"http://k.yimg.jp/images/top/sp2/cmn/logo-ns-131205.png")
+        let req = NSURLRequest(URL:url!)
+        
+        NSURLConnection.sendAsynchronousRequest(req, queue:NSOperationQueue.mainQueue()){(res, data, err) in
+
+            let image = UIImage(data:data)
+            self.imgLogo.image = image
+        }
     }
 
     override func didReceiveMemoryWarning() {
